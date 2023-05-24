d44d2725fc89196215f49ee189660235de25dcde
matsa_007 <serg.arbitran@gmail.com>
2023-05-24 23:14:59 +0300

    no message

---
+++ ModifiedableTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ModifiedableTable
+//
+//  Created by Сергей Матвеенко on 24.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .gray
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
