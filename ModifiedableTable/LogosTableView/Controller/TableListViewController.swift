a4d30d74a9257270661c45f3c33cdff5a1ac122d
matsa_007 <serg.arbitran@gmail.com>
2023-05-26 00:15:19 +0300

    Creating and adding of displayData in table added

--- ModifiedableTable/LogosTableView/Controller/TableListViewController.swift
+++ ModifiedableTable/LogosTableView/Controller/TableListViewController.swift
@@ -24,17 +24,16 @@ final class TableListViewController: UIViewController {
     
     private lazy var logosTitlesDescriotionsTable: UITableView = {
         let table = UITableView()
-        table.backgroundColor = .red
         return table
     }()
     
-    // MARK: - Initialisation
+    // MARK: - Initialization
     
     init(logosList: LogosList) {
         self.logosList = logosList.getLogosList()
         super.init(nibName: nil, bundle: nil)
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -43,6 +42,7 @@ final class TableListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.createDisplayData()
         self.view.addSubview(self.logosTitlesDescriotionsTable)
         self.logosTitlesDescriotionsTable.frame = self.view.bounds
         self.logosTitlesDescriotionsTable.register(TableViewListCell.self, forCellReuseIdentifier: "tableViewCell")
@@ -50,18 +50,27 @@ final class TableListViewController: UIViewController {
         self.logosTitlesDescriotionsTable.delegate = self
         
     }
-
+    
+    // MARK: - DATA updating
+    
+    private func createDisplayData() {
+        let displayDataCreator = DisplayDataCreator()
+        self.displayData = displayDataCreator.createDisplaydataData(displayData: self.displayData, logosList: self.logosList, titleText: "Title", descriptionText: "Description", maxCount: 999)
+    }
+    
 }
 
 // MARK: - Extensions
 
 extension TableListViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        3
+        self.displayData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "tableViewCell", for: indexPath) as? TableViewListCell else { return UITableViewCell() }
+        let displayData = self.displayData[indexPath.row]
+        cell.setCellView(displayData: DisplayData(logoName: displayData.logoName, title: displayData.title, description: displayData.description))
         return cell
     }
 }
--- ModifiedableTable/LogosTableView/View/TableViewListCell.swift
+++ ModifiedableTable/LogosTableView/View/TableViewListCell.swift
@@ -7,15 +7,86 @@
 
 import UIKit
 
-class TableViewListCell: UITableViewCell {
+final class TableViewListCell: UITableViewCell {
+    
+    // MARK: - GUI
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
+    private lazy var logoView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    private lazy var commonLabelsStackView : UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        return stackView
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "HelveticaNeue-Bold", size: 17)
+        return label
+    }()
+    
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    // MARK: - Lifecycle
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.addSubviews()
+        self.setConstrains()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.logoView.image = nil
+        self.titleLabel.text = nil
+        self.descriptionLabel.text = nil
+    }
+    
+    // MARK: - Constraints
+    
+    private func addSubviews() {
+        self.contentView.addSubview(self.logoView)
+        self.contentView.addSubview(self.commonLabelsStackView)
+        self.commonLabelsStackView.addSubview(self.titleLabel)
+        self.commonLabelsStackView.addSubview(self.descriptionLabel)
+    }
+    
+    private func setConstrains() {
+        self.logoView.snp.makeConstraints {
+            $0.top.bottom.left.equalToSuperview()
+            $0.width.equalToSuperview().dividedBy(3)
+        }
+        
+        self.commonLabelsStackView.snp.makeConstraints {
+            $0.top.bottom.right.equalToSuperview()
+            $0.left.equalTo(self.logoView.snp.right)
+        }
+        
+        self.titleLabel.snp.makeConstraints {
+            $0.top.left.right.equalToSuperview()
+            $0.height.equalToSuperview().dividedBy(2)
+        }
+        
+        self.descriptionLabel.snp.makeConstraints {
+            $0.top.equalTo(self.titleLabel.snp.bottom)
+            $0.bottom.left.right.equalToSuperview()
+        }
+    }
+    
+    // MARK: - UI updating
+    
+    func setCellView(displayData: TableListViewController.DisplayData) {
+        self.logoView.image = UIImage(systemName: displayData.logoName)
+        self.titleLabel.text = displayData.title
+        self.descriptionLabel.text = displayData.description
     }
 
 }
